23077912e3b4aa2262b9eead5fcd1511ce3da5be
GRZ4NA <grzanatv@gmail.com>
2021-06-19 19:04:25 +0200

    Main class + README.md

--- src/DiscordCommands.ts
+++ src/DiscordCommands.ts
@@ -1,1 +1,20 @@
-import { Client } from 'discord.js';
+//IMPORTS
+import { Client, ClientOptions } from 'discord.js';
+
+//TYPE DEFINITIONS
+interface ConstructorOptions {
+    name: string,
+    prefix: string,
+    helpCommand?: boolean,
+    clientOptions?: ClientOptions
+}
+
+//MAIN CLASS
+class Bot {
+    name: string;
+    client: Client;
+    constructor(options: ConstructorOptions) {
+        this.name = options.name;
+        this.client = new Client(options.clientOptions);
+    }
+}
