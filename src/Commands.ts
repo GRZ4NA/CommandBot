16b5548d9bb16f85bb4c842d423df79b50fe3d43
GRZ4NA <grzanatv@gmail.com>
2021-06-20 17:06:07 +0200

    return types response

--- src/Commands.ts
+++ src/Commands.ts
@@ -54,7 +54,13 @@ class Command {
     async start(message?: Message, cmdArguments?: string[]) {
         const memberPermissions : Readonly<Permissions> = message?.member?.permissions || new Permissions(0);
         if(memberPermissions.any(this.permissions, true)) {
-            await this.function(message, cmdArguments);
+            const fnResult = await this.function(message, cmdArguments);
+            if(typeof fnResult == 'string') {
+                await message?.reply(fnResult);
+            }
+            else if(fnResult instanceof MessageEmbed) {
+                await message?.channel.send(fnResult);
+            }
         }
         else {
             throw new PermissionsError(this, message?.member);
