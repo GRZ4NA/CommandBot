23f72796356e9fabea5e2235207ab7a2a38fc5db
GRZ4NA <grzanatv@gmail.com>
2021-09-02 22:51:47 +0200

    fixed types importing in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import { MessageEmbed } from "discord.js";
 import { Bot } from "./src/Bot.js";
 import { Command } from "./src/Command.js";
-import { InitOptions, CommandBuilder, CommandMessageStructure, HelpMessageParams, ParameterResolvable, ParameterType } from "./src/types.js";
-import { ObjectID, InputParameter, ParameterSchema, StringParameter, BooleanParameter, NumberParameter, ObjectParameter, Parameter } from "./src/Parameter.js";
-import { SystemMessageAppearance, SystemMessageData } from "./src/SystemMessage.js";
+import { InitOptions } from "./src/types/Bot.js";
+import { CommandBuilder, CommandMessageStructure } from "./src/types/Command.js";
+import { HelpMessageParams } from "./src/types/HelpMessage.js";
+import { ParameterResolvable, ParameterType, ParameterSchema } from "./src/types/Parameter.js";
+import { SystemMessageAppearance, SystemMessageData } from "./src/types/SystemMessage.js";
+import { ObjectID, InputParameter, StringParameter, BooleanParameter, NumberParameter, ObjectParameter, Parameter } from "./src/Parameter.js";
 import { PermissionsError, ParameterTypeError, MissingParameterError, OperationSuccess } from "./src/errors.js";
-import { MessageEmbed } from "discord.js";
 
 export default Bot;
 export {
