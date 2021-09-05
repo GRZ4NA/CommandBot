903168ddd2fd0a91a8855fdc56da9d27c2921bd1
GRZ4NA <grzanatv@gmail.com>
2021-09-05 18:02:20 +0200

    added registered api type

--- src/types/api.ts
+++ src/types/api.ts
@@ -22,3 +22,14 @@ export interface TextCommandOptionChoiceObject {
     name: string;
     value: string;
 }
+
+export interface RegisteredCommandObject {
+    id: string;
+    type?: number;
+    application_id: string;
+    guild_id?: string;
+    name: string;
+    description: string;
+    options?: TextCommandOptionObject[];
+    default_permission?: boolean;
+}
