f92997416ddc2af7fb9d2f77d2b674c91630a1f6
GRZ4NA <grzanatv@gmail.com>
2021-09-08 21:32:12 +0200

    added TargetID to CommandInteractionData

--- src/commands/types/commands.ts
+++ src/commands/types/commands.ts
@@ -4,10 +4,11 @@ import { MessageCommand } from "../MessageCommand.js";
 import { UserCommand } from "../UserCommand.js";
 import { Interaction, Message } from "discord.js";
 import { ParameterResolvable } from "structures/types/Parameter.js";
+import { TargetID } from "structures/Parameter.js";
 
 export type CommandResolvable = BaseCommand | ChatCommand | MessageCommand | UserCommand;
 
 export interface CommandInteractionData<T extends Interaction | Message> {
     command: T extends Interaction ? BaseCommand : ChatCommand;
-    parameters: ReadonlyMap<string, ParameterResolvable>;
+    parameters: ReadonlyMap<string, ParameterResolvable> | TargetID;
 }
