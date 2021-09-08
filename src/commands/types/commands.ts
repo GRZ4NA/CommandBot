608b0141d0ef7f80956cf0b7c049fb6a273b4ab8
GRZ4NA <grzanatv@gmail.com>
2021-09-08 22:53:08 +0200

    moved general command-related types

--- src/commands/types/commands.ts
+++ src/commands/types/commands.ts
@@ -1,10 +1,17 @@
 import { BaseCommand } from "../BaseCommand.js";
 import { ChatCommand } from "../ChatCommand.js";
-import { MessageCommand } from "../ContextMenuCommand.js";
+import { ContextMenuCommand } from "../ContextMenuCommand.js";
 import { ParameterResolvable } from "structures/types/Parameter.js";
 import { TargetID } from "structures/parameter.js";
+import { CommandInteraction, Message, MessageEmbed, ReplyMessageOptions } from "discord.js";
 
-export type CommandResolvable = BaseCommand | ChatCommand | MessageCommand;
+export type CommandResolvable = BaseCommand | ChatCommand | ContextMenuCommand;
+
+export type CommandType = "CHAT" | "MESSAGE" | "USER";
+
+export type CommandFunctionReturnTypes = void | string | MessageEmbed | ReplyMessageOptions | Promise<void | string | MessageEmbed | ReplyMessageOptions>;
+
+export type CommandFunction = (args: ReadonlyMap<string, ParameterResolvable> | TargetID, interaction?: Message | CommandInteraction) => CommandFunctionReturnTypes;
 
 export interface CommandInteractionData {
     command: BaseCommand;
