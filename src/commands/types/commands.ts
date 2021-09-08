e76d951d96d7f35f4e60618e086085cf30dc9f7f
GRZ4NA <grzanatv@gmail.com>
2021-09-08 20:47:58 +0200

    changed CommandInteractionData to ReadonlyMap

--- src/commands/types/commands.ts
+++ src/commands/types/commands.ts
@@ -2,12 +2,12 @@ import { BaseCommand } from "../BaseCommand.js";
 import { ChatCommand } from "../ChatCommand.js";
 import { MessageCommand } from "../MessageCommand.js";
 import { UserCommand } from "../UserCommand.js";
-import { InputParameter } from "../../structures/Parameter.js";
 import { Interaction, Message } from "discord.js";
+import { ParameterResolvable } from "structures/types/Parameter.js";
 
 export type CommandResolvable = BaseCommand | ChatCommand | MessageCommand | UserCommand;
 
 export interface CommandInteractionData<T extends Interaction | Message> {
     command: T extends Interaction ? BaseCommand : ChatCommand;
-    parameters: InputParameter[];
+    parameters: ReadonlyMap<string, ParameterResolvable>;
 }
