002d6a19395e6909964ee6583e43c7f8fb3e53e4
GRZ4NA <grzanatv@gmail.com>
2021-09-08 20:43:02 +0200

    added dev state to indicate early versions of CommandBot

--- src/structures/StateManager.ts
+++ src/structures/StateManager.ts
@@ -1,5 +1,6 @@
 export class StateManager {
     private _isRunning: boolean = false;
+    private readonly _unstable: boolean = true;
 
     get running() {
         return this._isRunning;
@@ -9,5 +10,9 @@ export class StateManager {
         if (!this._isRunning) {
             this._isRunning = bool;
         }
+    }
+
+    get dev() {
+        return this._unstable;
     }
 }
