037a4a599963a84425c6906d06679002d12aa771
GRZ4NA <grzanatv@gmail.com>
2021-09-08 19:07:16 +0200

    added prefix and argumentSeparator to CommandManager

--- src/commands/CommandManager.ts
+++ src/commands/CommandManager.ts
@@ -2,4 +2,11 @@ import { BaseCommand } from "./BaseCommand.js";
 
 export class CommandManager {
     private readonly _commands: BaseCommand[] = [];
+    public readonly prefix?: string;
+    public readonly argumentSeparator: string;
+
+    constructor(prefix?: string, argSep?: string) {
+        this.prefix = prefix;
+        this.argumentSeparator = argSep || ",";
+    }
 }
