1f3878e7b9140238fcde9f0a3bdd2ffb4d674299
GRZ4NA <grzanatv@gmail.com>
2021-09-12 18:30:21 +0200

    changed main exports to exclude command classes

--- index.ts
+++ index.ts
@@ -1,28 +1,26 @@
 import { Bot } from "./src/structures/Bot.js";
-import { ChatCommand } from "./src/commands/ChatCommand.js";
-import { ContextMenuCommand } from "./src/commands/ContextMenuCommand.js";
 import { InitOptions } from "./src/structures/types/Bot.js";
 import { ChatCommandInit } from "./src/commands/types/ChatCommand.js";
+import { ContextMenuCommandInit } from "./src/commands/types/ContextMenuCommand.js";
+import { NestedCommandInit } from "./src/commands/types/NestedCommand.js";
+import { SubCommandInit } from "./src/commands/types/SubCommand.js";
+import { SubCommandGroupInit } from "./src/commands/types/SubCommandGroup.js";
 import { HelpMessageParams } from "./src/commands/types/HelpMessage.js";
 import { ParameterResolvable, ParameterType, ParameterSchema } from "./src/structures/types/Parameter.js";
 import { SystemMessageAppearance, SystemMessageData } from "./src/structures/types/SystemMessage.js";
 import { ObjectID, Parameter } from "./src/structures/parameter.js";
 import { PermissionsError, ParameterTypeError, MissingParameterError, OperationSuccess } from "./src/errors.js";
 import { CommandInteractionData } from "./src/commands/types/commands.js";
-import { NestedCommand } from "./src/commands/NestedCommand.js";
-import { SubCommandGroup } from "./src/commands/SubCommandGroup.js";
-import { SubCommand } from "./src/commands/SubCommand.js";
 
 export default Bot;
 export {
     Bot,
-    ChatCommand,
-    ContextMenuCommand,
-    NestedCommand,
-    SubCommandGroup,
-    SubCommand,
     InitOptions,
     ChatCommandInit,
+    ContextMenuCommandInit,
+    NestedCommandInit,
+    SubCommandGroupInit,
+    SubCommandInit,
     CommandInteractionData,
     HelpMessageParams,
     ParameterResolvable,
