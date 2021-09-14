fedebf0b23c7db71e1d2a161846cd9214eaaa0eb
GRZ4NA <grzanatv@gmail.com>
2021-09-14 23:53:41 +0200

    removed extra line

--- src/commands/types/ChatCommand.ts
+++ src/commands/types/ChatCommand.ts
@@ -5,7 +5,6 @@ import { BaseCommandInit } from "./BaseCommand.js";
  * @interface
  * Options for building a {@link Command} object
  */
-
 export interface ChatCommandInit extends BaseCommandInit {
     parameters?: ParameterSchema[] | "simple" | "no_input";
     aliases?: string[] | string;
