7c9c2c8b8b8afbdb60e81bd3eb028bdc6fe922d8
GRZ4NA <grzanatv@gmail.com>
2021-09-20 19:29:42 +0200

    added toObject method and guild and dm parameter checking

--- src/commands/SubCommand.ts
+++ src/commands/SubCommand.ts
@@ -1,10 +1,13 @@
 import { generateUsageFromArguments } from "../utils/generateUsageFromArguments.js";
-import { DefaultParameter, Parameter } from "../structures/parameter.js";
+import { DefaultParameter, Parameter, TargetID } from "../structures/parameter.js";
 import { PermissionCommand } from "./base/PermissionCommand.js";
 import { NestedCommand } from "./NestedCommand.js";
 import { SubCommandGroup } from "./SubCommandGroup.js";
 import { CommandRegExps } from "./types/commands.js";
 import { SubCommandInit } from "./types/InitOptions.js";
+import { ChatCommandObject, ChatCommandOptionObject, ChatCommandOptionType, TextCommandOptionChoiceObject } from "../structures/types/api.js";
+import { ParameterResolvable } from "../structures/types/Parameter.js";
+import { Interaction, Message } from "discord.js";
 
 export class SubCommand extends PermissionCommand {
     private readonly _parent: SubCommandGroup | NestedCommand;
@@ -49,5 +52,83 @@ export class SubCommand extends PermissionCommand {
         if (this.description && !CommandRegExps.chatDescription.test(this.description)) {
             throw new Error(`The description of "${this.name}" doesn't match a regular expression ${CommandRegExps.chatDescription}`);
         }
+    }
+
+    public async start(args: ReadonlyMap<string, ParameterResolvable>, interaction: Message | Interaction, target?: TargetID): Promise<void> {
+        if (this._parent instanceof SubCommandGroup ? !this._parent.parent.dm && !interaction.guild : !this._parent.dm && !interaction.guild)
+            throw new Error(`Command "${this.name}" is only available inside a guild.`);
+        if (
+            this._parent instanceof SubCommandGroup
+                ? this._parent.parent.guilds && this._parent.parent.guilds.length > 0 && !this._parent.parent.guilds.find((id) => id === interaction.guild?.id)
+                : this._parent.guilds && this._parent.guilds.length > 0 && !this._parent.guilds.find((id) => id === interaction.guild?.id)
+        )
+            throw new Error(`Command "${this.name}" is not available.`);
+        await super.start(args, interaction, target);
+    }
+
+    public toObject(): ChatCommandObject {
+        const obj: ChatCommandObject = {
+            ...super.toObject(),
+            type: 1 as 1,
+            description: this.description,
+        };
+        let options: ChatCommandOptionObject[] = [];
+        if (this.parameters) {
+            options = this.parameters
+                .map((p) => {
+                    let type = 3;
+                    switch (p.type) {
+                        case "boolean":
+                            type = 5;
+                            break;
+                        case "user":
+                            type = 6;
+                            break;
+                        case "channel":
+                            type = 7;
+                            break;
+                        case "role":
+                            type = 8;
+                            break;
+                        case "mentionable":
+                            type = 9;
+                            break;
+                        case "number":
+                            type = 10;
+                            break;
+                        case "target":
+                            throw new Error(`"target" parameter cannot be used in chat commands`);
+                        default:
+                            type = 3;
+                            break;
+                    }
+                    const choices: TextCommandOptionChoiceObject[] = [];
+                    if (p.choices) {
+                        p.choices.map((c) => {
+                            choices.push({ name: c, value: c });
+                        });
+                    }
+                    const optionObj: ChatCommandOptionObject = {
+                        name: p.name,
+                        description: p.description,
+                        required: !p.optional,
+                        type: p.choices ? 3 : (type as ChatCommandOptionType),
+                        choices: choices.length > 0 ? choices : undefined,
+                    };
+                    return optionObj;
+                })
+                .sort((a, b) => {
+                    if (a.required && !b.required) {
+                        return -1;
+                    } else if (a.required && b.required) {
+                        return 0;
+                    } else if (!a.required && b.required) {
+                        return 1;
+                    }
+                    return 0;
+                });
+            obj.options = options;
+        }
+        return obj;
     }
 }
