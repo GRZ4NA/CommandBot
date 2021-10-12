d2d3103c37069ef3cb1ef861ccfb2c462caecfcc
GRZ4NA <grzanatv@gmail.com>
2021-10-12 17:27:41 +0200

    JSDoc (CommandPermissions, InputManager)

--- src/structures/CommandPermissions.ts
+++ src/structures/CommandPermissions.ts
@@ -3,8 +3,22 @@ import { PermissionCommand } from "../commands/base/PermissionCommand.js";
 import { CommandPermissionsInit, PermissionCheckTypes, PermissionFunction } from "../commands/types/permissions.js";
 
 export class CommandPermissions {
+    /**
+     * Command bound to this object
+     * @type {PermissionCommand}
+     */
     public readonly command: PermissionCommand;
+
+    /**
+     * If set to "ALL", the command can only be used when all specified conditions are met. (works only for Discord.js permissions object)
+     * @type {PermissionCheckTypes}
+     */
     public readonly permissions: Permissions | PermissionFunction;
+
+    /**
+     * Command permissions
+     * @type {Permissions | PermissionFunction}
+     */
     public readonly checkType: PermissionCheckTypes;
 
     constructor(command: PermissionCommand, o?: CommandPermissionsInit) {
@@ -13,14 +27,27 @@ export class CommandPermissions {
         this.permissions = o?.resolvable instanceof Function ? o.resolvable : new Permissions(o?.resolvable ?? BigInt(0));
     }
 
+    /**
+     * Whether this object uses custom function to check permissions
+     * @type {boolean}
+     */
     get isCustom(): boolean {
         return this.permissions instanceof Function ? true : false;
     }
 
+    /**
+     * Permission bitfield
+     * @type {BigInt}
+     */
     get bitfield(): BigInt {
         return this.permissions instanceof Function ? BigInt(NaN) : BigInt(this.permissions.bitfield);
     }
 
+    /**
+     *
+     * @param {Interaction | Message} i - command interaction or Discord message
+     * @returns {boolean} Whether a sender can use the command bound to this object
+     */
     public check(i: Interaction | Message): boolean {
         if (this.permissions instanceof Function) {
             return this.permissions(i);
--- src/structures/InputManager.ts
+++ src/structures/InputManager.ts
@@ -5,11 +5,37 @@ import { InputParameter, TargetID } from "./parameter.js";
 import { InputParameterValue } from "./types/Parameter";
 
 export class InputManager {
+    /**
+     * Command related to this manager
+     * @type {FunctionCommand}
+     */
     public readonly command: FunctionCommand;
+
+    /**
+     * Command interaction or message
+     * @type {Interaction | Message}
+     */
     public readonly interaction: Interaction | Message;
+
+    /**
+     * Command target (when using context menu interactions)
+     * @type {TargetID<any>}
+     */
     public readonly target?: TargetID<any>;
+
+    /**
+     * All input arguments
+     * @type {InputParameter<any>[]}
+     */
     private readonly arguments: InputParameter<any>[];
 
+    /**
+     * @constructor
+     * @param {FunctionCommand} command - command related to this manager
+     * @param {Interaction | Message} interaction  - interaction or message
+     * @param {InputManager<any>[]} args - list of input arguments
+     * @param {TargetID<any>} target - interaction target (when using context menu interactions)
+     */
     constructor(command: FunctionCommand, interaction: Interaction | Message, args: InputParameter<any>[], target?: TargetID<any>) {
         this.command = command;
         this.interaction = interaction;
@@ -17,6 +43,12 @@ export class InputManager {
         this.target = target;
     }
 
+    /**
+     * Get input values
+     * @param query - parameter name
+     * @param type - parameter type
+     * @returns {ParameterResolvable} Argument value bound to a parameter
+     */
     public get<T extends ParameterType>(query: string, type: T): InputParameterValue<T> | null {
         return (this.arguments.filter((arg) => arg.type === type).find((arg) => arg.name === query)?.value as InputParameterValue<T>) ?? null;
     }
