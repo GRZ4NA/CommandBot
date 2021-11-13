31fcaa52108b3c6cd06368097a51e48a5615b857
GRZ4NA <grzanatv@gmail.com>
2021-11-13 03:04:11 +0100

    added a huge amount of main exports

--- index.ts
+++ index.ts
@@ -1,15 +1,74 @@
 import { Bot } from "./src/structures/Bot.js";
 import { InitOptions } from "./src/structures/types/Bot.js";
-import { ChatCommandInit, ContextMenuCommandInit, SubCommandInit, SubCommandGroupInit } from "./src/commands/types/InitOptions.js";
+import {
+    ChatCommandInit,
+    ContextMenuCommandInit,
+    SubCommandInit,
+    SubCommandGroupInit,
+    APICommandInit,
+    FunctionCommandInit,
+    GuildCommandInit,
+    PermissionCommandInit,
+    PermissionGuildCommandInit,
+} from "./src/commands/types/InitOptions.js";
 import { HelpMessageParams } from "./src/commands/types/HelpMessage.js";
-import { ParameterResolvable, ParameterType, ParameterSchema } from "./src/structures/types/Parameter.js";
-import { SystemMessageAppearance, SystemMessageData } from "./src/structures/types/SystemMessage.js";
-import { ObjectID, TargetID, Parameter } from "./src/structures/parameter.js";
+import {
+    ParameterResolvable,
+    ParameterType,
+    ParameterSchema,
+    ObjectIdType,
+    TargetType,
+    InputParameterValue,
+    ObjectIdReturnType,
+    TargetIdReturnType,
+} from "./src/structures/types/Parameter.js";
+import { MessageType, SystemMessageAppearance, SystemMessageData } from "./src/structures/types/SystemMessage.js";
+import { ObjectID, TargetID, Parameter, InputParameter } from "./src/structures/parameter.js";
 import { PermissionsError, ParameterTypeError, MissingParameterError, OperationSuccess } from "./src/errors.js";
-import { ChildCommandType, Commands, ChildCommands, CommandInit, ChildCommandInit, CommandType } from "./src/commands/types/commands.js";
-import { CommandPermissionsInit } from "./src/commands/types/permissions.js";
-import { APICommandType } from "./src/structures/types/api.js";
+import {
+    ChildCommandType,
+    Commands,
+    ChildCommands,
+    CommandInit,
+    ChildCommandInit,
+    CommandType,
+    CommandFunction,
+    ContextType,
+    ChildCommandResolvable,
+    BaseCommands,
+    CommandFunctionReturnTypes,
+    BaseCommandType,
+} from "./src/commands/types/commands.js";
+import { CommandPermissionsInit, PermissionCheckTypes, PermissionFunction } from "./src/commands/types/permissions.js";
+import {
+    APICommandObject,
+    APICommandType,
+    ChatCommandObject,
+    ChatCommandOptionObject,
+    ChatCommandOptionType,
+    CommandPermission,
+    CommandPermissionType,
+    ContextMenuCommandObject,
+    RegisteredCommandObject,
+    SubCommandGroupObject,
+    TextCommandOptionChoiceObject,
+} from "./src/structures/types/api.js";
 import { InputManager } from "./src/structures/InputManager.js";
+import { SubCommand } from "./src/commands/SubCommand.js";
+import { SubCommandGroup } from "./src/commands/SubCommandGroup.js";
+import { ChatCommand } from "./src/commands/ChatCommand.js";
+import { ContextMenuCommand } from "./src/commands/ContextMenuCommand.js";
+import { FunctionCommand } from "./src/commands/base/FunctionCommand.js";
+import { Command } from "./src/commands/base/Command.js";
+import { CommandManager } from "./src/structures/CommandManager.js";
+import { SystemMessageManager } from "./src/structures/SystemMessage.js";
+import { CommandPermissions } from "./src/structures/CommandPermissions.js";
+import { HelpMessage } from "./src/commands/Help.js";
+import { PrefixManager } from "./src/structures/PrefixManager.js";
+import { GuildCommand } from "./src/commands/base/GuildCommand.js";
+import { PermissionCommand } from "./src/commands/base/PermissionCommand.js";
+import { PermissionGuildCommand } from "./src/commands/base/PermissionGuildCommand.js";
+import { ScopeResolvable } from "./src/structures/types/PrefixManager.js";
 
 export default Bot;
 export {
@@ -41,4 +100,49 @@ export {
     ChildCommands,
     CommandInit,
     ChildCommandInit,
+    SubCommand,
+    SubCommandGroup,
+    ChatCommand,
+    ContextMenuCommand,
+    ObjectIdType,
+    InputParameter,
+    FunctionCommand,
+    Command,
+    TargetType,
+    CommandManager,
+    CommandFunction,
+    PermissionCheckTypes,
+    PermissionFunction,
+    ContextType,
+    InputParameterValue,
+    ObjectIdReturnType,
+    TargetIdReturnType,
+    SystemMessageManager,
+    ChildCommandResolvable,
+    CommandPermissions,
+    RegisteredCommandObject,
+    HelpMessage,
+    PrefixManager,
+    BaseCommands,
+    ChatCommandObject,
+    APICommandObject,
+    ContextMenuCommandObject,
+    SubCommandGroupObject,
+    CommandFunctionReturnTypes,
+    BaseCommandType,
+    APICommandInit,
+    CommandPermission,
+    FunctionCommandInit,
+    MessageType,
+    GuildCommand,
+    PermissionCommand,
+    PermissionGuildCommand,
+    ChatCommandOptionObject,
+    CommandPermissionType,
+    ScopeResolvable,
+    TextCommandOptionChoiceObject,
+    ChatCommandOptionType,
+    GuildCommandInit,
+    PermissionCommandInit,
+    PermissionGuildCommandInit,
 };
